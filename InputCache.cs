fdb02dcb43ff8fcf83d95a3bf5bdc3ef474c7efd
Liux <2244695293@qq.com>
2025-08-08 17:17:41 +0800

    输入

---
+++ InputCache.cs
@@ -0,0 +1,25 @@
+﻿
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Threading;
+
+public class InputData
+{
+    public int MoveYaw;
+    public bool Key_Interaction_Pressed;
+}
+public static class InputCache
+{
+    private static volatile InputData _currentInput;
+
+    public static void SetInput(InputData inputData)
+    {
+        _currentInput = inputData;
+    }
+
+    public static InputData GetInput()
+    {
+        return _currentInput;
+    }
+}
+
